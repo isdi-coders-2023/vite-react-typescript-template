4a79cf10c053d404208f2b1f044e0c8d50a91154
Mario González <mario@mariogl.com>
2023-05-08 18:31:52 +0200

    Remove jest config file

--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testMatch: ["**/src/**/*.test.{ts,tsx}"],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-};
